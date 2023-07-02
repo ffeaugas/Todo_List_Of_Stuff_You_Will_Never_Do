ed690cb9bbd493126d5be300ebbb66e2ff1f3adf
ffeaugas <ffeaugas@student.42angouleme.fr>
2023-07-02 19:33:17 +0200

    Finished most of JS features :
    - Task filter selectors
    - Fixed cross and input behaviours
    - Adding dark mode
    
    Remaining
    
    - Drag and drop tasks feature
    - Removing an annoying visible line in darkmode
    - Fixing input text and paragraphs overflows

--- script.js
+++ script.js
@@ -1,70 +1,142 @@
+const inputText = document.querySelector('input[type="text"]');
+const inputCheckbox = document.querySelector('form input[type="checkbox"]');
+const tasklist = document.getElementById('tasklist');
+const clearComplete = document.getElementById('task_clearer');
+const taskCheckedCounter = document.getElementById('task_count');
+const filterButtons = document.querySelectorAll('button');
+const lightModeButton = document.getElementById('light_mode_icon');
+const darkModeButton = document.getElementById('dark_mode_icon');
 
-/*  Cross Button    */
+/* Functions */
 
-const crossButtons = document.querySelectorAll('.cross_button');
-
-crossButtons.forEach(crossButton => {
-    crossButton.addEventListener('click', function () {
-        const task_li = crossButton.parentNode;
-        task_li.remove();
-        updateTastCheckedCounter();
-    })
-})
-
-/* Task checked counter */
-
-const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-
-checkboxes.forEach(checkbox => {
-    checkbox.addEventListener('click', function () {
-        updateTastCheckedCounter();
-    })
-})
-
-function updateTastCheckedCounter() {
-    const taskCheckedCounter = document.getElementById('task_count');
-    const checkboxesArray = Array.from(document.querySelectorAll('input[type="checkbox"]'));
+function updateTaskCheckedCounter() {
+    const checkboxesArray = Array.from(document.querySelectorAll('ul input[type="checkbox"]'));
     let counter = checkboxesArray.reduce(function (count, checkbox) {
         if (checkbox.checked)
-            return (count + 1);
-        return (count);
+            return (count);
+        return (count + 1);
     }, 0);
-    console.log(counter);
-    let res = counter + ' items left';
-    taskCheckedCounter.textContent = res;
+    if (!counter)
+        taskCheckedCounter.textContent = 'No item left';
+    else if (counter === 1)
+        taskCheckedCounter.textContent = counter + ' item left';
+    else
+        taskCheckedCounter.textContent = counter + ' items left';
 }
 
-/* Text input */
+/* Dark/light mode event */
 
-const tasklist = document.getElementById('tasklist');
+darkModeButton.addEventListener('click', function () {
+    document.querySelector('body').classList.add('dark_color');
+    darkModeButton.classList.add('hide');
+    lightModeButton.classList.remove('hide');
+})
 
-const input = document.querySelector('input[type="text"]');
+lightModeButton.addEventListener('click', function () {
+    document.querySelector('body').classList.remove('dark_color');
+    darkModeButton.classList.remove('hide');
+    lightModeButton.classList.add('hide');
+})
 
-input.addEventListener('keydown', function (event) {
+/* Clear complete event */
+
+clearComplete.addEventListener('click', function () {
+    const tasks = document.querySelectorAll('ul .task_li');
+    tasks.forEach(task => {
+        if (task.querySelector('input').checked)
+            task.remove();
+    })
+    updateTaskCheckedCounter();
+})
+
+/* Selector Buttons */
+
+filterButtons.forEach(button => {
+    button.addEventListener('click', event => {
+        event.preventDefault();
+        const filter = button.getAttribute('data-filter');
+        button.parentNode.querySelector('.selected').classList.remove('selected');
+        button.classList.add('selected');
+        if (filter === 'active') {
+            tasklist.classList.add('hide_completed');
+            tasklist.classList.remove('hide_active');
+        }
+        else if (filter === 'completed') {
+            tasklist.classList.add('hide_active');
+            tasklist.classList.remove('hide_completed');
+        }
+        else {
+            tasklist.classList.remove('hide_active');
+            tasklist.classList.remove('hide_completed');
+        }
+    })
+})
+
+/* Text input event */
+
+inputText.addEventListener('keydown', function (event) {
     if (event.keyCode === 13) {
         event.preventDefault();
-        let enteredText = input.value;
-        newTask = createTask(enteredText);
-        tasklist.appendChild(newTask);
+        newTask = new Task(inputText.value, inputCheckbox.checked);
+        newTask.appendTo(tasklist);
+        inputText.value = '';
     }
 });
 
-function createTask(enteredText) {
+/* Class */
 
-    const newTask = document.createElement('li');
-    newTask.classList.add('task_li');
-    const label = document.createElement('label');
-    label.classList.add('checkbox');
-    const inputt = document.createElement('input');
-    inputt.type = 'checkbox';
-    label.appendChild(inputt);
-    newTask.appendChild(label);
-    const p = document.createElement('p');
-    p.textContent = enteredText;
-    newTask.appendChild(p);
-    const cross = document.createElement('b');
-    cross.classList.add('cross_button');
-    cross.innerHTML = '&#215';
-    newTask.appendChild(cross);
-    return newTask;
+class Task {
+    #element
+
+    constructor(inputText, isChecked) {
+        const newTask = document.createElement('li');
+        newTask.classList.add('task_li');
+        this.#element = newTask;
+
+        const label = document.createElement('label');
+        label.classList.add('checkbox');
+
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+
+        const p = document.createElement('p');
+        p.textContent = inputText;
+
+        const cross = document.createElement('b');
+        cross.classList.add('cross_button');
+        cross.innerHTML = '&#215';
+
+        label.appendChild(checkbox);
+        newTask.appendChild(label);
+        newTask.appendChild(p);
+        newTask.appendChild(cross);
+
+        if (isChecked) {
+            checkbox.checked = true;
+            newTask.classList.add('is_completed');
+            p.classList.add('p_checked');
+        }
+
+        checkbox.addEventListener('click', function () {
+            updateTaskCheckedCounter();
+            if (checkbox.checked) {
+                newTask.classList.add('is_completed');
+                p.classList.add('p_checked');
+            }
+            else {
+                newTask.classList.remove('is_completed');
+                p.classList.remove('p_checked');
+            }
+        })
+
+        cross.addEventListener('click', function () {
+            newTask.remove();
+            updateTaskCheckedCounter();
+        })
+    }
+
+    appendTo(element) {
+        element.appendChild(this.#element);
+        updateTaskCheckedCounter();
+    }
 }
